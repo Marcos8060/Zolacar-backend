8a45485ded06094d56d7d1d37171938a5fb8d2f9
Marcos8060 <marcosgav80@gmail.com>
2022-05-26 23:23:44 +0300

    added a featured model

--- api/admin.py
+++ api/admin.py
@@ -3,3 +3,5 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Car)
+admin.site.register(Featured)
+
--- api/models.py
+++ api/models.py
@@ -17,3 +17,16 @@ class Car(models.Model):
 
     def __str__(self):
         return self.name
+
+class Featured(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=30)
+    price = models.CharField(max_length=10)
+    luggage = models.CharField(max_length=20)
+    doors = models.CharField(max_length=10)
+    passengers = models.CharField(max_length=10)
+    transmission = models.CharField(max_length=30)
+    gas = models.CharField(max_length=30)
+    interior1 = models.ImageField(upload_to='images/')
+    interior2 = models.ImageField(upload_to='images/')
+    interior3 = models.ImageField(upload_to='images/')
