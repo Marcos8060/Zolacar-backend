7c7079eafe4b3e7efb10229a11b32889b09468d5
Marcos8060 <marcosgav80@gmail.com>
2022-05-26 23:08:35 +0300

    installed the cloudinary package

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Car(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(upload_to='images/')
     name = models.CharField(max_length=30)
     price = models.CharField(max_length=10)
     luggage = models.CharField(max_length=20)
@@ -11,9 +11,9 @@ class Car(models.Model):
     passengers = models.CharField(max_length=10)
     transmission = models.CharField(max_length=30)
     gas = models.CharField(max_length=30)
-    interior1 = models.ImageField()
-    interior2 = models.ImageField()
-    interior3 = models.ImageField()
+    interior1 = models.ImageField(upload_to='images/')
+    interior2 = models.ImageField(upload_to='images/')
+    interior3 = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.name
