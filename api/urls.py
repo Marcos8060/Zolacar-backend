155f8337949c09666c437520f690837780409102
Marcos8060 <marcosgav80@gmail.com>
2022-05-26 23:34:57 +0300

    created two views with corresponding routes

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('cars/',CarList.as_view()),
+    path('featured/',FeaturedList.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import *
+from .serializer import *
 
 # Create your views here.
+class CarList(generics.ListAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
+
+class FeaturedList(generics.ListAPIView):
+    queryset = Featured.objects.all()
+    serializer_class = FeatureSerializer
 
