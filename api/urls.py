192dd7dd1261d9a4ff0e56e4a92adaa0f0d2fb6d
Marcos8060 <marcosgav80@gmail.com>
2022-05-27 00:11:16 +0300

    created detail view

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('cars/',CarList.as_view()),
+    path('cars/<int:pk>/',CarDetail.as_view()),
     path('featured/',FeaturedList.as_view()),
+    path('featured/<int:pk>/',FeatureDetail.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -4,12 +4,19 @@ from .models import *
 from .serializer import *
 
 # Create your views here.
-class CarList(generics.ListAPIView):
+class CarList(generics.ListCreateAPIView):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
 
+class CarDetail(generics.RetrieveUpdateAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
 
-class FeaturedList(generics.ListAPIView):
+class FeaturedList(generics.ListCreateAPIView):
+    queryset = Featured.objects.all()
+    serializer_class = FeatureSerializer
+
+class FeatureDetail(generics.RetrieveUpdateAPIView):
     queryset = Featured.objects.all()
     serializer_class = FeatureSerializer
 
