42efef4c3594638015a6e8d0900e5261f2de998b
Marcos8060 <marcosgav80@gmail.com>
2022-06-06 16:18:37 +0300

    customized my token claims to capture the username

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import *
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 
@@ -12,7 +11,7 @@ urlpatterns = [
     path('featured/',FeaturedList.as_view()),
     path('featured/<int:pk>/',FeatureDetail.as_view()),
     path('cars/search/',CarFleetDetailFilter.as_view()),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('register/',CustomUserCreate.as_view(),name='create_user'),
--- api/views.py
+++ api/views.py
@@ -8,6 +8,24 @@ from rest_framework.decorators import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 # Create your views here.
